f86e7765a18b97346547d3127edc394d6b231dc3
sahil <118107400+chamooda2@users.noreply.github.com>
2023-09-20 22:10:45 +0530

    added functionality to check top domains

--- somthing.py
+++ somthing.py
@@ -1,23 +1,25 @@
 import csv
 
-domain_list =[]
 
+#adding csv to list
+domain_list =[]
 csv_file_path = "top200.csv"
 
 with open(csv_file_path, mode='r', newline='') as file:
     
     csv_reader = csv.DictReader(file)
-
     for row in csv_reader:
-
         domain = row["domain"]
         domain_list.append(domain)
-
 #print(domain_list)
+#-----------------------------------------------------------------------#
+
 
 def is_one_char_change(str1, str2):
     """
     Check if two strings are one character change away.
+
+    Can add if it is in top domains in this 
     """
     if len(str1) != len(str2):
         return False
@@ -36,14 +38,19 @@ def string_belongs_to_list(input_str, string_list):
     Check if input_str belongs to a list of strings with one character change.
     """
     for item in string_list:
-        if is_one_char_change(input_str, item):
-            return True
-    return False
+        if item == input_str:
+            return 1
+        elif(is_one_char_change(input_str, item)):
+            return 2
+    return 0
 
 # Input string to check
-current_domain = "goggle.com"
+current_domain = "g0ogle.com"
 
-if string_belongs_to_list(current_domain, domain_list):
+if string_belongs_to_list(current_domain, domain_list)==1:
+    print(f"'{current_domain}' is top domain")
+elif string_belongs_to_list(current_domain, domain_list)==2:
     print(f"'{current_domain}' could be a typo")
 else:
     print(f"'{current_domain}' not a typo")
+
