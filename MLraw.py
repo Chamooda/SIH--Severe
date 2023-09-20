7be8da34cf42750e3463872aa513e079750355a5
KayTee424 <128174326+KayTee424@users.noreply.github.com>
2023-09-21 00:34:57 +0530

    Update MLraw.py
    updated relevant code

--- MLraw.py
+++ MLraw.py
@@ -85,29 +85,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-# import whois
-
-# def get_whois_info(domain_name):
-#     try:
-#         # Query WHOIS information for the domain
-#         whois_info = whois.whois(domain_name)
-        
-#         # Print the WHOIS information
-#         print(whois_info)
-        
-#         # You can also access specific WHOIS fields like this:
-#         # print("Domain Name:", whois_info.domain_name)
-#         # print("Registrar:", whois_info.registrar)
-#         # print("Creation Date:", whois_info.creation_date)
-#         # ...
-        
-#     except whois.parser.PywhoisError as e:
-#         print("Error:", e)
-
-# # Replace 'example.com' with the domain you want to query
-# domain_name = 'google.com'
-
-# get_whois_info(domain_name)
